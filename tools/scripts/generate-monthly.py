c9ac1f13195f1741f5dfa0f2bee758d396b5ca6a
The MadDoG.tmdg <105843577+teremuhamblin@users.noreply.github.com>
2026-04-24 22:39:54 +0200

    Create generate-monthly.py

---
+++ tools/scripts/generate-monthly.py
@@ -0,0 +1,12 @@
+template = """\
+Monthly Review
+==============
+
+Bilan du mois :
+Progrès réalisés :
+Difficultés rencontrées :
+Ajustements :
+"""
+
+with open("monthly_review.rst", "w") as f:
+    f.write(template)
