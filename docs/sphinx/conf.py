da1f6b38674742ae578b3c12ec627900ceed7953
The MadDoG.tmdg <105843577+teremuhamblin@users.noreply.github.com>
2026-04-24 23:05:21 +0200

    Update conf.py

--- docs/sphinx/conf.py
+++ docs/sphinx/conf.py
@@ -1,6 +1,11 @@
 project = "MySelf"
-extensions = ["sphinx.ext.autodoc"]
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.todo",
+    "sphinx.ext.githubpages",
+]
 templates_path = ["_templates"]
 exclude_patterns = []
 html_static_path = ["_static"]
-html_css_files = ["theme.css"]
+html_theme = "myself_theme"
+html_css_files = ["theme/theme.css"]
